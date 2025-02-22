a1af38a16726376b31ed8fbe8abd8fbd7c809306
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2025-02-22 08:48:01 +0100

    removed src from checkout

--- download-openedge.sh
+++ download-openedge.sh
@@ -2,12 +2,12 @@
 echo base: ${OPENEDGE_BASE_VERSION}
 echo version: ${OPENEDGE_VERSION}
 
-docker run -v ${PWD}/src:/target devbfvio/oeinstaller:${OPENEDGE_VERSION}
+docker run -v ${PWD}:/target devbfvio/oeinstaller:${OPENEDGE_VERSION}
 
 if [[ "${OPENEDGE_VERSION}" > "12.8.3" ]]; then
   echo ">=12.8.4 we need the base version as well"
-  mv ${PWD}/src/PROGRESS_OE.tar.gz ${PWD}/src/PROGRESS_PATCH_OE.tar.gz
+  mv ${PWD}/PROGRESS_OE.tar.gz ${PWD}/PROGRESS_PATCH_OE.tar.gz
   echo "download base version ${OPENEDGE_BASE_VERSION}"
-  docker run -v ${PWD}/src:/target devbfvio/oeinstaller:${OPENEDGE_BASE_VERSION}
-  ls -l ${PWD}/src
+  docker run -v ${PWD}:/target devbfvio/oeinstaller:${OPENEDGE_BASE_VERSION}
+  ls -l ${PWD}
 fi
